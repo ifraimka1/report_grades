63d1b06b6b10cc225c7da887a018a388fb6c0e7e
ifraimka <mr.ifraim@yandex.ru>
2025-01-30 17:14:11 +0300

    Добавил русский язык

---
+++ lang/ru/report_grades.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Строки плагина на русском языке.
+ *
+ * @package     report_grades
+ * @category    string
+ * @copyright   2025 Solomonov Ifraim mr.ifraim@yandex.ru
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Отчет по оценкам';
+$string['select_cohort'] = 'Выберите группу';
+$string['select_semestr'] = 'Выберите семестр';
+$string['export'] = 'Скачать';
+$string['all_groups'] = 'Все группы';
+
+$string['tabhead_student'] = 'ФИО';
+$string['tabhead_cohort'] = 'Группа';
+$string['tabhead_email'] = 'Адрес электронной почты';
